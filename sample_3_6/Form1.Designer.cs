236b52233a2fd76b96374845bcedd451f32001f1
nirzaf <mfmfazrin1986@gmail.com>
2022-05-10 13:29:33 +0530

    Add project files.

---
+++ sample_3_6/Form1.Designer.cs
@@ -0,0 +1,397 @@
+﻿namespace sample_3_6 {
+  partial class Form1 {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.components = new System.ComponentModel.Container();
+      DevExpress.XtraBars.Docking2010.Views.Tabbed.DockingContainer dockingContainer1 = new DevExpress.XtraBars.Docking2010.Views.Tabbed.DockingContainer();
+      DevExpress.XtraBars.Docking2010.Views.Tabbed.DockingContainer dockingContainer2 = new DevExpress.XtraBars.Docking2010.Views.Tabbed.DockingContainer();
+      this.barButtonItem1 = new DevExpress.XtraBars.BarButtonItem();
+      this.barSubItem1 = new DevExpress.XtraBars.BarSubItem();
+      this.barSubItem2 = new DevExpress.XtraBars.BarSubItem();
+      this.barButtonItem2 = new DevExpress.XtraBars.BarButtonItem();
+      this.barButtonItem3 = new DevExpress.XtraBars.BarButtonItem();
+      this.barSubItem3 = new DevExpress.XtraBars.BarSubItem();
+      this.barMdiChildrenListItem1 = new DevExpress.XtraBars.BarMdiChildrenListItem();
+      this.ribbonControl1 = new DevExpress.XtraBars.Ribbon.RibbonControl();
+      this.barButtonItem4 = new DevExpress.XtraBars.BarButtonItem();
+      this.ribbonPage1 = new DevExpress.XtraBars.Ribbon.RibbonPage();
+      this.ribbonPageGroup1 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+      this.ribbonPageGroup2 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+      this.ribbonPageGroup4 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+      this.ribbonPageGroup5 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+      this.ribbonPageGroup3 = new DevExpress.XtraBars.Ribbon.RibbonPageGroup();
+      this.ribbonStatusBar1 = new DevExpress.XtraBars.Ribbon.RibbonStatusBar();
+      this.documentManager1 = new DevExpress.XtraBars.Docking2010.DocumentManager(this.components);
+      this.tabbedView1 = new DevExpress.XtraBars.Docking2010.Views.Tabbed.TabbedView(this.components);
+      this.childFormDocument = new DevExpress.XtraBars.Docking2010.Views.Tabbed.Document(this.components);
+      this.documentGroup1 = new DevExpress.XtraBars.Docking2010.Views.Tabbed.DocumentGroup(this.components);
+      this.secondMDIChildDocument = new DevExpress.XtraBars.Docking2010.Views.Tabbed.Document(this.components);
+      this.documentGroup2 = new DevExpress.XtraBars.Docking2010.Views.Tabbed.DocumentGroup(this.components);
+      this.dockManager1 = new DevExpress.XtraBars.Docking.DockManager(this.components);
+      this.dockPanel1 = new DevExpress.XtraBars.Docking.DockPanel();
+      this.dockPanel1_Container = new DevExpress.XtraBars.Docking.ControlContainer();
+      this.dockPanel2 = new DevExpress.XtraBars.Docking.DockPanel();
+      this.dockPanel2_Container = new DevExpress.XtraBars.Docking.ControlContainer();
+      this.dockPanel3 = new DevExpress.XtraBars.Docking.DockPanel();
+      this.dockPanel3_Container = new DevExpress.XtraBars.Docking.ControlContainer();
+      this.panelContainer1 = new DevExpress.XtraBars.Docking.DockPanel();
+      ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.documentManager1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.tabbedView1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.childFormDocument)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.documentGroup1)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.secondMDIChildDocument)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.documentGroup2)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).BeginInit();
+      this.dockPanel1.SuspendLayout();
+      this.dockPanel2.SuspendLayout();
+      this.dockPanel3.SuspendLayout();
+      this.panelContainer1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // barButtonItem1
+      // 
+      this.barButtonItem1.Caption = "Create MDI Child";
+      this.barButtonItem1.Id = 0;
+      this.barButtonItem1.Name = "barButtonItem1";
+      this.barButtonItem1.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem1_ItemClick);
+      // 
+      // barSubItem1
+      // 
+      this.barSubItem1.Caption = "&File";
+      this.barSubItem1.Id = 1;
+      this.barSubItem1.Name = "barSubItem1";
+      // 
+      // barSubItem2
+      // 
+      this.barSubItem2.Caption = "&View";
+      this.barSubItem2.Id = 2;
+      this.barSubItem2.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem2),
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItem3, true)});
+      this.barSubItem2.MergeType = DevExpress.XtraBars.BarMenuMerge.MergeItems;
+      this.barSubItem2.Name = "barSubItem2";
+      // 
+      // barButtonItem2
+      // 
+      this.barButtonItem2.Caption = "First Main Menu Button";
+      this.barButtonItem2.Id = 3;
+      this.barButtonItem2.Name = "barButtonItem2";
+      // 
+      // barButtonItem3
+      // 
+      this.barButtonItem3.Caption = "Button At End";
+      this.barButtonItem3.Id = 4;
+      this.barButtonItem3.MergeOrder = 100;
+      this.barButtonItem3.Name = "barButtonItem3";
+      // 
+      // barSubItem3
+      // 
+      this.barSubItem3.Caption = "Window";
+      this.barSubItem3.Id = 5;
+      this.barSubItem3.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.barMdiChildrenListItem1)});
+      this.barSubItem3.Name = "barSubItem3";
+      // 
+      // barMdiChildrenListItem1
+      // 
+      this.barMdiChildrenListItem1.Caption = "barMdiChildrenListItem1";
+      this.barMdiChildrenListItem1.Id = 6;
+      this.barMdiChildrenListItem1.Name = "barMdiChildrenListItem1";
+      // 
+      // ribbonControl1
+      // 
+      this.ribbonControl1.ExpandCollapseItem.Id = 0;
+      this.ribbonControl1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.ribbonControl1.ExpandCollapseItem,
+            this.ribbonControl1.SearchEditItem,
+            this.barButtonItem1,
+            this.barSubItem1,
+            this.barSubItem2,
+            this.barButtonItem2,
+            this.barButtonItem3,
+            this.barSubItem3,
+            this.barMdiChildrenListItem1,
+            this.barButtonItem4});
+      this.ribbonControl1.Location = new System.Drawing.Point(0, 0);
+      this.ribbonControl1.MaxItemId = 2;
+      this.ribbonControl1.MdiMergeStyle = DevExpress.XtraBars.Ribbon.RibbonMdiMergeStyle.Always;
+      this.ribbonControl1.Name = "ribbonControl1";
+      this.ribbonControl1.Pages.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPage[] {
+            this.ribbonPage1});
+      this.ribbonControl1.Size = new System.Drawing.Size(902, 158);
+      this.ribbonControl1.StatusBar = this.ribbonStatusBar1;
+      // 
+      // barButtonItem4
+      // 
+      this.barButtonItem4.Caption = "Create Second MDI Child";
+      this.barButtonItem4.Id = 1;
+      this.barButtonItem4.Name = "barButtonItem4";
+      this.barButtonItem4.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItem4_ItemClick);
+      // 
+      // ribbonPage1
+      // 
+      this.ribbonPage1.Groups.AddRange(new DevExpress.XtraBars.Ribbon.RibbonPageGroup[] {
+            this.ribbonPageGroup1,
+            this.ribbonPageGroup2,
+            this.ribbonPageGroup4,
+            this.ribbonPageGroup5,
+            this.ribbonPageGroup3});
+      this.ribbonPage1.Name = "ribbonPage1";
+      this.ribbonPage1.Text = "MainMenu";
+      // 
+      // ribbonPageGroup1
+      // 
+      this.ribbonPageGroup1.Name = "ribbonPageGroup1";
+      this.ribbonPageGroup1.Text = "File";
+      // 
+      // ribbonPageGroup2
+      // 
+      this.ribbonPageGroup2.ItemLinks.Add(this.barButtonItem2);
+      this.ribbonPageGroup2.ItemLinks.Add(this.barButtonItem3, true);
+      this.ribbonPageGroup2.Name = "ribbonPageGroup2";
+      this.ribbonPageGroup2.Text = "View";
+      // 
+      // ribbonPageGroup4
+      // 
+      this.ribbonPageGroup4.ItemLinks.Add(this.barButtonItem1);
+      this.ribbonPageGroup4.ItemLinks.Add(this.barButtonItem4);
+      this.ribbonPageGroup4.Name = "ribbonPageGroup4";
+      this.ribbonPageGroup4.Text = "Tools";
+      // 
+      // ribbonPageGroup5
+      // 
+      this.ribbonPageGroup5.Name = "ribbonPageGroup5";
+      this.ribbonPageGroup5.Text = "Custom 5";
+      // 
+      // ribbonPageGroup3
+      // 
+      this.ribbonPageGroup3.ItemLinks.Add(this.barMdiChildrenListItem1);
+      this.ribbonPageGroup3.Name = "ribbonPageGroup3";
+      this.ribbonPageGroup3.Text = "Window";
+      // 
+      // ribbonStatusBar1
+      // 
+      this.ribbonStatusBar1.Location = new System.Drawing.Point(0, 573);
+      this.ribbonStatusBar1.Name = "ribbonStatusBar1";
+      this.ribbonStatusBar1.Ribbon = this.ribbonControl1;
+      this.ribbonStatusBar1.Size = new System.Drawing.Size(902, 24);
+      // 
+      // documentManager1
+      // 
+      this.documentManager1.MdiParent = this;
+      this.documentManager1.MenuManager = this.ribbonControl1;
+      this.documentManager1.View = this.tabbedView1;
+      this.documentManager1.ViewCollection.AddRange(new DevExpress.XtraBars.Docking2010.Views.BaseView[] {
+            this.tabbedView1});
+      // 
+      // tabbedView1
+      // 
+      this.tabbedView1.DocumentGroups.AddRange(new DevExpress.XtraBars.Docking2010.Views.Tabbed.DocumentGroup[] {
+            this.documentGroup1,
+            this.documentGroup2});
+      this.tabbedView1.Documents.AddRange(new DevExpress.XtraBars.Docking2010.Views.BaseDocument[] {
+            this.childFormDocument,
+            this.secondMDIChildDocument});
+      dockingContainer1.Element = this.documentGroup1;
+      dockingContainer2.Element = this.documentGroup2;
+      this.tabbedView1.RootContainer.Nodes.AddRange(new DevExpress.XtraBars.Docking2010.Views.Tabbed.DockingContainer[] {
+            dockingContainer1,
+            dockingContainer2});
+      // 
+      // childFormDocument
+      // 
+      this.childFormDocument.Caption = "ChildForm";
+      this.childFormDocument.ControlName = "ChildForm";
+      this.childFormDocument.ControlTypeName = "sample_3_6.ChildForm";
+      // 
+      // documentGroup1
+      // 
+      this.documentGroup1.Items.AddRange(new DevExpress.XtraBars.Docking2010.Views.Tabbed.Document[] {
+            this.childFormDocument});
+      // 
+      // secondMDIChildDocument
+      // 
+      this.secondMDIChildDocument.Caption = "SecondMDIChild";
+      this.secondMDIChildDocument.ControlName = "SecondMDIChild";
+      this.secondMDIChildDocument.ControlTypeName = "sample_3_6.SecondMDIChild";
+      // 
+      // documentGroup2
+      // 
+      this.documentGroup2.Items.AddRange(new DevExpress.XtraBars.Docking2010.Views.Tabbed.Document[] {
+            this.secondMDIChildDocument});
+      // 
+      // dockManager1
+      // 
+      this.dockManager1.Form = this;
+      this.dockManager1.RootPanels.AddRange(new DevExpress.XtraBars.Docking.DockPanel[] {
+            this.panelContainer1,
+            this.dockPanel3});
+      this.dockManager1.TopZIndexControls.AddRange(new string[] {
+            "DevExpress.XtraBars.BarDockControl",
+            "DevExpress.XtraBars.StandaloneBarDockControl",
+            "System.Windows.Forms.MenuStrip",
+            "System.Windows.Forms.StatusStrip",
+            "System.Windows.Forms.StatusBar",
+            "DevExpress.XtraBars.Ribbon.RibbonStatusBar",
+            "DevExpress.XtraBars.Ribbon.RibbonControl",
+            "DevExpress.XtraBars.Navigation.OfficeNavigationBar",
+            "DevExpress.XtraBars.Navigation.TileNavPane",
+            "DevExpress.XtraBars.TabFormControl",
+            "DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl",
+            "DevExpress.XtraBars.ToolbarForm.ToolbarFormControl"});
+      // 
+      // dockPanel1
+      // 
+      this.dockPanel1.Controls.Add(this.dockPanel1_Container);
+      this.dockPanel1.Dock = DevExpress.XtraBars.Docking.DockingStyle.Fill;
+      this.dockPanel1.ID = new System.Guid("60fb8b2d-07a4-4797-b1b7-e7083953c3a3");
+      this.dockPanel1.Location = new System.Drawing.Point(3, 26);
+      this.dockPanel1.Name = "dockPanel1";
+      this.dockPanel1.OriginalSize = new System.Drawing.Size(200, 200);
+      this.dockPanel1.Size = new System.Drawing.Size(193, 360);
+      this.dockPanel1.Text = "dockPanel1";
+      // 
+      // dockPanel1_Container
+      // 
+      this.dockPanel1_Container.Location = new System.Drawing.Point(0, 0);
+      this.dockPanel1_Container.Name = "dockPanel1_Container";
+      this.dockPanel1_Container.Size = new System.Drawing.Size(193, 360);
+      this.dockPanel1_Container.TabIndex = 0;
+      // 
+      // dockPanel2
+      // 
+      this.dockPanel2.Controls.Add(this.dockPanel2_Container);
+      this.dockPanel2.Dock = DevExpress.XtraBars.Docking.DockingStyle.Fill;
+      this.dockPanel2.ID = new System.Guid("f2cce4d8-f698-4e76-97ce-eefd4efb06c0");
+      this.dockPanel2.Location = new System.Drawing.Point(3, 26);
+      this.dockPanel2.Name = "dockPanel2";
+      this.dockPanel2.OriginalSize = new System.Drawing.Size(200, 200);
+      this.dockPanel2.Size = new System.Drawing.Size(193, 360);
+      this.dockPanel2.Text = "dockPanel2";
+      // 
+      // dockPanel2_Container
+      // 
+      this.dockPanel2_Container.Location = new System.Drawing.Point(0, 0);
+      this.dockPanel2_Container.Name = "dockPanel2_Container";
+      this.dockPanel2_Container.Size = new System.Drawing.Size(193, 360);
+      this.dockPanel2_Container.TabIndex = 0;
+      // 
+      // dockPanel3
+      // 
+      this.dockPanel3.Controls.Add(this.dockPanel3_Container);
+      this.dockPanel3.Dock = DevExpress.XtraBars.Docking.DockingStyle.Right;
+      this.dockPanel3.ID = new System.Guid("443daad9-41aa-460e-a3d3-f9fc76e147bd");
+      this.dockPanel3.Location = new System.Drawing.Point(702, 158);
+      this.dockPanel3.Name = "dockPanel3";
+      this.dockPanel3.OriginalSize = new System.Drawing.Size(200, 200);
+      this.dockPanel3.Size = new System.Drawing.Size(200, 415);
+      this.dockPanel3.Text = "dockPanel3";
+      // 
+      // dockPanel3_Container
+      // 
+      this.dockPanel3_Container.Location = new System.Drawing.Point(4, 26);
+      this.dockPanel3_Container.Name = "dockPanel3_Container";
+      this.dockPanel3_Container.Size = new System.Drawing.Size(193, 386);
+      this.dockPanel3_Container.TabIndex = 0;
+      // 
+      // panelContainer1
+      // 
+      this.panelContainer1.ActiveChild = this.dockPanel2;
+      this.panelContainer1.Controls.Add(this.dockPanel2);
+      this.panelContainer1.Controls.Add(this.dockPanel1);
+      this.panelContainer1.Dock = DevExpress.XtraBars.Docking.DockingStyle.Left;
+      this.panelContainer1.ID = new System.Guid("9069d44c-0e19-4f3a-b23a-586a90cc2e8b");
+      this.panelContainer1.Location = new System.Drawing.Point(0, 158);
+      this.panelContainer1.Name = "panelContainer1";
+      this.panelContainer1.OriginalSize = new System.Drawing.Size(200, 200);
+      this.panelContainer1.Size = new System.Drawing.Size(200, 415);
+      this.panelContainer1.Tabbed = true;
+      this.panelContainer1.Text = "panelContainer1";
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(902, 597);
+      this.Controls.Add(this.dockPanel3);
+      this.Controls.Add(this.panelContainer1);
+      this.Controls.Add(this.ribbonStatusBar1);
+      this.Controls.Add(this.ribbonControl1);
+      this.IsMdiContainer = true;
+      this.Name = "Form1";
+      this.Ribbon = this.ribbonControl1;
+      this.StatusBar = this.ribbonStatusBar1;
+      this.Text = "Form1";
+      ((System.ComponentModel.ISupportInitialize)(this.ribbonControl1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.documentManager1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.tabbedView1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.childFormDocument)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.documentGroup1)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.secondMDIChildDocument)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.documentGroup2)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).EndInit();
+      this.dockPanel1.ResumeLayout(false);
+      this.dockPanel2.ResumeLayout(false);
+      this.dockPanel3.ResumeLayout(false);
+      this.panelContainer1.ResumeLayout(false);
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+    private DevExpress.XtraBars.BarButtonItem barButtonItem1;
+    private DevExpress.XtraBars.BarSubItem barSubItem1;
+    private DevExpress.XtraBars.BarSubItem barSubItem2;
+    private DevExpress.XtraBars.BarButtonItem barButtonItem2;
+    private DevExpress.XtraBars.BarButtonItem barButtonItem3;
+    private DevExpress.XtraBars.BarSubItem barSubItem3;
+    private DevExpress.XtraBars.BarMdiChildrenListItem barMdiChildrenListItem1;
+    private DevExpress.XtraBars.Ribbon.RibbonControl ribbonControl1;
+    private DevExpress.XtraBars.Ribbon.RibbonPage ribbonPage1;
+    private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup1;
+    private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup2;
+    private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup3;
+    private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup4;
+    private DevExpress.XtraBars.Ribbon.RibbonPageGroup ribbonPageGroup5;
+    private DevExpress.XtraBars.Ribbon.RibbonStatusBar ribbonStatusBar1;
+    private DevExpress.XtraBars.BarButtonItem barButtonItem4;
+    private DevExpress.XtraBars.Docking2010.DocumentManager documentManager1;
+    private DevExpress.XtraBars.Docking2010.Views.Tabbed.TabbedView tabbedView1;
+    private DevExpress.XtraBars.Docking2010.Views.Tabbed.DocumentGroup documentGroup1;
+    private DevExpress.XtraBars.Docking2010.Views.Tabbed.Document childFormDocument;
+    private DevExpress.XtraBars.Docking2010.Views.Tabbed.Document secondMDIChildDocument;
+    private DevExpress.XtraBars.Docking.DockPanel dockPanel3;
+    private DevExpress.XtraBars.Docking.ControlContainer dockPanel3_Container;
+    private DevExpress.XtraBars.Docking.DockPanel panelContainer1;
+    private DevExpress.XtraBars.Docking.DockPanel dockPanel2;
+    private DevExpress.XtraBars.Docking.ControlContainer dockPanel2_Container;
+    private DevExpress.XtraBars.Docking.DockPanel dockPanel1;
+    private DevExpress.XtraBars.Docking.ControlContainer dockPanel1_Container;
+    private DevExpress.XtraBars.Docking2010.Views.Tabbed.DocumentGroup documentGroup2;
+    private DevExpress.XtraBars.Docking.DockManager dockManager1;
+  }
+}
+
---
+++ sample_3_6/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace sample_3_6 {
+  public partial class Form1 : DevExpress.XtraBars.Ribbon.RibbonForm {
+    public Form1() {
+      InitializeComponent();
+      // Handling the QueryControl event that will populate all automatically generated Documents
+      this.tabbedView1.QueryControl += tabbedView1_QueryControl;
+    }
+
+    private void barButtonItem1_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
+      var child = new ChildForm();
+      var doc = tabbedView1.AddDocument(child);
+      doc.Caption = $"Child Form number {tabbedView1.Documents.Count}";
+      doc.Properties.AllowClose = DevExpress.Utils.DefaultBoolean.False;
+    }
+
+    private void barManager1_Merge(object sender, DevExpress.XtraBars.BarManagerMergeEventArgs e) {
+      /*      if (e.ChildManager != null) {
+              var mainFormChildBar = barManager1.Bars["Child Bar"];
+              var childBar = e.ChildManager.Bars["Tools"];
+              mainFormChildBar.Merge(childBar);
+              childBar.Visible = false;
+              mainFormChildBar.Visible = true;
+            }*/
+    }
+
+    private void barManager1_UnMerge(object sender, DevExpress.XtraBars.BarManagerMergeEventArgs e) {
+      /*      var mainFormChildBar = barManager1.Bars["Child Bar"];
+            mainFormChildBar.UnMerge();
+            mainFormChildBar.Visible = false;*/
+    }
+
+    private void barButtonItem4_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e) {
+      var child = new SecondMDIChild();
+      child.MdiParent = this;
+      child.Show();
+    }
+
+    // Assigning a required content for each auto generated Document
+    void tabbedView1_QueryControl(object sender, DevExpress.XtraBars.Docking2010.Views.QueryControlEventArgs e) {
+
+      if (e.Document == childFormDocument)
+        e.Control = new sample_3_6.ChildForm();
+      if (e.Document == secondMDIChildDocument)
+        e.Control = new sample_3_6.SecondMDIChild();
+      if (e.Control == null)
+        e.Control = new System.Windows.Forms.Control();
+    }
+  }
+}
